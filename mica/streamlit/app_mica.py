2265063fcd4846f5ce4860bdd5132acc6a6c56b4
mariasanzb <142232678+mariasanzb@users.noreply.github.com>
2024-11-28 17:06:02 -0300

    Update app_mica.py

--- mica/streamlit/app_mica.py
+++ mica/streamlit/app_mica.py
@@ -4,7 +4,7 @@ import pandas as pd
 import math
 import os
 
-with open('modelo_recomendacion.pkl', 'rb') as file:
+with open('recommend_model.pkl.pkl', 'rb') as file:
     data = pickle.load(file)
 
 df_business = data['df_business']  # Data de negocios
